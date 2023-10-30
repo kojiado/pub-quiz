4bfe62b93d3f4d6e50ed96faaa63fdb77824ea56
kojiado <kojiado@outlook.com>
2023-10-30 15:27:32 +0100

    Skeleton UI

--- app/quiz/[id]/loading.js
+++
@@ -1,5 +0,0 @@
-import QuizSlideshowSkeleton from "@/components/skeleton/QuizSlideshowSkeleton"
-
-export default function Loading() {
-  return <QuizSlideshowSkeleton/>
-}
--- components/quiz/QuizSlideshow.jsx
+++ components/quiz/QuizSlideshow.jsx
@@ -5,13 +5,13 @@ import { useQuiz } from "@/hooks/useQuiz";
 
 import Question from '@/components/quiz/Question';
 import Button from '@/components/ui/Button';
-import { Suspense } from 'react';
+import QuizSlideshowSkeleton from '../skeleton/QuizSlideshowSkeleton';
 
 import { Splide, SplideSlide, } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 
 export default function QuizSlideshow({id}) {
-  const currentQuiz = useQuiz(id);
+  const { quiz, isLoading} = useQuiz(id);
   const splideRef = useRef(null);
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
   const [totalSlides, setTotalSlides] = useState(0);
@@ -28,7 +28,7 @@ export default function QuizSlideshow({id}) {
         });
       }
     }
-  }, []);
+  }, [quiz]);
 
   const goToNextSlide = () => {
     splideRef.current.splide.go('+1');
@@ -39,31 +39,35 @@ export default function QuizSlideshow({id}) {
   };
 
   useEffect(() => {
-    setTotalSlides(currentQuiz?.questions.length);
-  }, [currentQuiz])
+    setTotalSlides(quiz?.questions.length);
+  }, [quiz])
 
   return (
     <>
-      <div className="flex flex-col gap-[60px] pb-[160px]">
-        <h2 className='text-white text-[20px] underline'>Kviz: <span className="font-bold">{currentQuiz?.name}</span></h2>
-        <Splide
-          ref={splideRef}
-          options={{
-            pagination: false,
-            gap: 30,
-            arrows: false,
-            drag: false,
-          }}
-        >
-          {currentQuiz?.questions?.map((question,index)=>(
-            <Question
-              question={question.question}
-              answer={question.answer}
-              questionNum={index+1}
-            />
-          ))}
-        </Splide>
-      </div>
+      {isLoading ? (
+        <QuizSlideshowSkeleton/>
+      ):(
+        <div className="flex flex-col gap-[60px] pb-[160px]">
+          <h2 className='text-white text-[20px] underline'>Kviz: <span className="font-bold">{quiz?.name}</span></h2>
+          <Splide
+            ref={splideRef}
+            options={{
+              pagination: false,
+              gap: 30,
+              arrows: false,
+              drag: false,
+            }}
+          >
+            {quiz?.questions?.map((question,index)=>(
+              <Question
+                question={question.question}
+                answer={question.answer}
+                questionNum={index+1}
+              />
+            ))}
+          </Splide>
+        </div>
+      )}
       <div className='w-full h-auto p-[20px] backdrop-blur-md bg-[rgba(255,255,255,0.05)] fixed left-0 bottom-0 border-b-[1px] border-t-[1px] border-solid border-[#4F4C4D] flex items-center justify-between'>
         <Button
           style="secondary"
